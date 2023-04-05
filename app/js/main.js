9983685c14e13808a4ed30bb9411239011f9a03b
veterdelay <alxrorel0100@gmail.com>
2023-04-05 22:30:37 +0300

    git changes commit

---
+++ app/js/main.js
@@ -0,0 +1,22 @@
+//Header cart to top-header
+
+function cartToTop(){
+    let cart = $('.header-cart');
+    let screenSize = screen.width;
+    let topPosition = $('.header-body-content');
+    let bottomPosition = $('.header-bottom-interactive');
+
+    if(screenSize < 768){
+        cart.addClass('small-cart');
+        topPosition.append(cart);
+        cart.find(bottomPosition).remove();
+    }else{
+        cart.removeClass('small-cart');
+        bottomPosition.append(cart);
+        cart.find(topPosition).remove();
+    }
+}
+
+$( document ).ready( cartToTop );
+$( window ).on( "resize", cartToTop );
+
