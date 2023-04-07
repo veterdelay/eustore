fed58f4e8517723253a5654cfd9b9552432e999e
veterdelay <alxrorel0100@gmail.com>
2023-04-07 23:26:41 +0300

    git new commit

--- app/js/main.js
+++ app/js/main.js
@@ -19,5 +19,58 @@ function cartToTop(){
 }
 
 $( document ).ready( cartToTop );
-$( window ).on( "resize", cartToTop );
+$( window ).on( 'resize', cartToTop );
 
+//Open/Close header catalog (static)
+
+function openCatalog(){
+
+    let catalogWrapper = $('.catalog-default');
+    let catalogButton = $('.header-catalog');
+
+    catalogWrapper.toggleClass('catalog-close').toggleClass('catalog-open');
+    catalogButton.toggleClass('catalog-close').toggleClass('catalog-open');
+    
+}
+
+$('.header-catalog').on( 'click', openCatalog );
+
+//Close header catalog 
+
+function closeCatalog(){
+
+    let catalogWrapper = $('.catalog-default');
+    let catalogButton = $('.header-catalog');
+    let target = event.target;
+
+    if(catalogWrapper.hasClass('catalog-open') && !catalogButton.is(target) && !catalogButton.children().is(target)){
+        if ( !catalogWrapper.is(target) && catalogWrapper.has(target).length === 0 ) {
+            catalogWrapper.removeClass('catalog-open').addClass('catalog-close');
+            catalogButton.removeClass('catalog-open').addClass('catalog-close');
+        }
+    }
+
+}
+
+$( document ).click( closeCatalog );
+
+//Sticky header and menu
+
+function stickyHeader(){
+    
+    let stickyElement = $('.sticky-header');
+    let stickyElementStart = $('header');
+
+    if($(window).scrollTop() > stickyElementStart.offset().top + stickyElementStart.height()){
+        stickyElement.removeClass('sticky-noanimate');
+        stickyElement.addClass('sticky');
+    }
+
+    if($(window).scrollTop() < stickyElementStart.offset().top + stickyElementStart.height()){
+        stickyElement.addClass('sticky-noanimate');
+        stickyElement.removeClass('sticky');
+    }
+
+}
+
+$( window ).scroll( stickyHeader );
